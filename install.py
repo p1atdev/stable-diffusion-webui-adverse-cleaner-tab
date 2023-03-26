3172ede29040125b827f493881386e6e438a2b94
Plat <git@p1at.dev>
2023-03-26 22:24:56 +0900

    fix the install script

--- install.py
+++ install.py
@@ -1,7 +1,13 @@
 import launch
 
-if not launch.is_installed("cv2.ximgproc"):
+if not launch.is_installed("opencv-contrib-python"):
     launch.run_pip(
-        "install opencv-contrib-python --user",
+        "install opencv-contrib-python",
         "requirements for Adverse Cleaner",
     )
+
+# if not launch.is_installed("opencv-contrib-python"):
+#     launch.run_pip(
+#         "install opencv-contrib-python --user",
+#         "requirements for Adverse Cleaner",
+#     )
